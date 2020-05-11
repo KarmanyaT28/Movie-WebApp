ed94a485a1f2589958d986b1abd6367820ad397d
karmanya-crypto <61420434+karmanya-crypto@users.noreply.github.com>
2020-05-11 15:31:22 +0530

    Update edit_post.php

--- admin/include/edit_post.php
+++ admin/include/edit_post.php
@@ -1,80 +1,80 @@
 <?php
 
     if(isset($_GET['p_id'])){
-    
-    $the_post_id =  $_GET['p_id'];
+   
+    $the_movie_id =  $_GET['p_id'];
 
     }
 
 
-    $query = "SELECT * FROM posts WHERE post_id = $the_post_id  ";
+    $query = "SELECT * FROM posts WHERE movie_id = $the_movie_id  ";
     $select_posts_by_id = mysqli_query($connection,$query);  
 
     while($row = mysqli_fetch_assoc($select_posts_by_id)) {
-        $post_id            = $row['post_id'];
-        $post_author = $row['post_author'];
-        $post_title         = $row['post_title'];
-        $post_category_id   = $row['post_category_id'];
-        $post_status        = $row['post_status'];
-        $post_image         = $row['post_image'];
-        $post_content       = $row['post_content'];
-        $post_tag          = $row['post_tag'];
-        $post_comment_count = $row['post_comment_count'];
-        $post_date          = $row['post_date'];
-        
+        $movie_id            = $row['movie_id'];
+        $movie_actor = $row['movie_actor'];
+        $movie_name         = $row['movie_name'];
+        $movie_category_id   = $row['movie_category_id'];
+        $movie_language        = $row['movie_language'];
+        $image         = $row['image'];
+        $content       = $row['content'];
+        $movie_tags          = $row['movie_tags'];
+        $watched_count = $row['watched_count'];
+        $date_of_release          = $row['date_of_release'];
+       
          }
 
 
     if(isset($_POST['update_post'])) {
-        
-        
-        $post_author           =  $_POST['post_author'];
-        $post_title          =  $_POST['post_title'];
-        $post_category_id    =  $_POST['post_category'];
-        $post_status         =  $_POST['post_status'];
-        $post_image          =  $_FILES['image']['name'];
-        $post_image_temp     =  $_FILES['image']['tmp_name'];
-        $post_content        =  $_POST['post_content'];
-        $post_tag           =  $_POST['post_tag'];
-        
-        move_uploaded_file($post_image_temp, "../images/$post_image"); 
-        
-        if(empty($post_image)) {
-        
-        $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
+       
+       
+        $movie_actor           =  $_POST['movie_actor'];
+        $movie_name          =  $_POST['movie_name'];
+        $movie_category_id    =  $_POST['movie_category'];
+        $movie_language         =  $_POST['movie_language'];
+        $image          =  $_FILES['image']['name'];
+        $image_temp     =  $_FILES['image']['tmp_name'];
+        $content        =  $_POST['content'];
+        $movie_tags          =  $_POST['movie_tags'];
+       
+        move_uploaded_file($image_temp, "../images/$image");
+       
+        if(empty($image)) {
+       
+        $query = "SELECT * FROM posts WHERE movie_id = $the_movie_id ";
         $select_image = mysqli_query($connection,$query);
-            
+           
         while($row = mysqli_fetch_array($select_image)) {
-            
-           $post_image = $row['post_image'];
-        
+           
+           $image = $row['image'];
+       
         }
-        
-        
+       
+       
 }
-        $post_title = mysqli_real_escape_string($connection, $post_title);
+        $movie_name = mysqli_real_escape_string($connection, $movie_name);
 
-        
+       
           $query = "UPDATE posts SET ";
-          $query .="post_title  = '{$post_title}', ";
-          $query .="post_category_id = '{$post_category_id}', ";
-          $query .="post_date   =  now(), ";
-          $query .="post_author = '{$post_author}', ";
-          $query .="post_status = '{$post_status}', ";
-          $query .="post_tag   = '{$post_tag}', ";
-          $query .="post_content= '{$post_content}', ";
-          $query .="post_image  = '{$post_image}' ";
-          $query .= "WHERE post_id = {$the_post_id} ";
-        
+          $query .="movie_name  = '{$movie_name}', ";
+          $query .="movie_category_id = '{$movie_category_id}', ";
+          $query .="date_of_release   =  now(), ";
+          $query .="movie_actor = '{$movie_actor}', ";
+          $query .="movie_language = '{$movie_language}', ";
+          $query .="movie_tags   = '{$movie_tags}', ";
+          $query .="content= '{$content}', ";
+          $query .="image  = '{$image}' ";
+          $query .= "WHERE movie_id = {$the_movie_id} ";
+       
         $update_post = mysqli_query($connection,$query);
-        
+       
         //confirmQuery($update_post);
-        
-        //echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
-        
+       
+        //echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_movie_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
+       
 
-    
-    
+   
+   
     }
 
 
@@ -90,26 +90,26 @@
      
      
       <div class="form-group">
-         <label for="title">Post Title</label>
-          <input value="<?php echo htmlspecialchars(stripslashes($post_title)); ?>"  type="text" class="form-control" name="post_title">
+         <label for="title">Movie Title</label>
+          <input value="<?php echo htmlspecialchars(stripslashes($movie_name)); ?>"  type="text" class="form-control" name="movie_name">
       </div>
 
          <div class="form-group">
-         <label for="post_author">Post Author</label>
-          <input value="<?php echo $post_author;?>" type="text" class="form-control" name="post_author">
+         <label for="movie_actor"> Actor </label>
+          <input value="<?php echo $movie_actor;?>" type="text" class="form-control" name="movie_actor">
       </div>
      
-          
-          
+         
+         
            <div class="form-group">
        <label for="categories">Categories</label>
-       <select name="post_category" id="">
+       <select name="movie_category" id="">
            
       <?php
 
         $query = "SELECT * FROM categories ";
         $select_categories = mysqli_query($connection,$query);
-        
+       
         //confirmQuery($select_categories);
 
 
@@ -118,9 +118,9 @@
         $cat_title = $row['cat_title'];
 
 
-        if($cat_id == $post_category_id) {
+        if($cat_id == $movie_category_id) {
 
-      
+     
         echo "<option selected value='{$cat_id}'>{$cat_title}</option>";
 
 
@@ -130,13 +130,13 @@
 
 
         }
-      
+     
    
         }
 
 ?>
            
-        
+       
        </select>
 
       </div>
@@ -144,33 +144,33 @@
        
 
      
-      
+     
       <div class="form-group">
-         <label for="post_status">Post Status</label>
-          <input value="<?php echo $post_status;?>" type="text" class="form-control" name="post_status">
+         <label for="movie_language">Movie Language</label>
+          <input value="<?php echo $movie_language;?>" type="text" class="form-control" name="movie_language">
       </div>
-      
+     
      
     <div class="form-group">
 
-       <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+       <img width="100" src="../images/<?php echo $image; ?>" alt="">
        <input  type="file" name="image">
       </div>
 
       <div class="form-group">
-         <label for="post_tag">Post Tags</label>
-          <input value="<?php echo $post_tag; ?>"  type="text" class="form-control" name="post_tag">
+         <label for="movie_tags">Movie TAGS</label>
+          <input value="<?php echo $movie_tags; ?>"  type="text" class="form-control" name="movie_tags">
       </div>
-      
+     
       <div class="form-group">
-         <label for="post_content">Post Content</label>
-         <textarea  class="form-control "name="post_content" id="body" cols="30" rows="10"><?php echo $post_content; ?>
+         <label for="content">Content</label>
+         <textarea  class="form-control "name="content" id="body" cols="30" rows="10"><?php echo $content; ?>
          
-        
+       
          </textarea>
       </div>
-      
-      
+     
+     
 
        <div class="form-group">
           <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
