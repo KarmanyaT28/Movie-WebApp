5fe9e5ceeea4381c7e0c800c88b10ffc9dea0b94
karmanya-crypto <61420434+karmanya-crypto@users.noreply.github.com>
2020-05-11 15:49:05 +0530

    Update widget.php

--- include/widget.php
+++ include/widget.php
@@ -1,4 +1,20 @@
 <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+                    <h2><b>Upcoming events and features</b></h2>
+                    <p>
+                        <ul style="font-size:120%;">
+                            <li>We will shortly be adding a recommender system recommending all users with amazing movies and features.</li>
+                            <br>
+                            <li>The ‘Latest’ feature will help people discover new and upcoming content they will love in a way that’s simple, fun and easy.</li>
+                            <br>
+                            <li>Jerry Seinfeld: 23 Hours to Kill (Comedy special)</li>
+                            <br>
+                            <li>Becoming (New documentary)</li>
+                            <br>
+                            
+                            <li>Have a Good Trip: Adventures in Psychedelics (New documentary)</li>
+                            <br>
+                            <li>SOON ROLLOUT A 'TOP 10' FEATURE THAT WILL SHOW USERS THE MOST POPULAR SHOWS, MOVIES IN THEIR COUNTRY</li>
+                        </ul>
+                    </p>
                 </div>
+
