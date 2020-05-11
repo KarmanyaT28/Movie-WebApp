b250b790a01794894c8b5f9a169c458e7bd2f562
karmanya-crypto <61420434+karmanya-crypto@users.noreply.github.com>
2020-05-11 15:55:12 +0530

    Update post.php

--- post.php
+++ post.php
@@ -22,44 +22,54 @@ include "include/navigation.php"
     
                 if(isset($_GET['p_id'])){
                     
-                  $the_post_id = $_GET['p_id'];  
+                  $the_movie_id = $_GET['p_id'];  
                     
                     
                     
                 }
     
-                $query="SELECT * FROM posts WHERE post_id=$the_post_id";
+                $query="SELECT * FROM posts WHERE movie_id=$the_movie_id";
                 $select_all_posts_query = mysqli_query($connection,$query);
                 
                 while($row = mysqli_fetch_assoc($select_all_posts_query)){
                    
-                    $post_title=  $row['post_title'];
-                    $post_author=  $row['post_author'];
-                    $post_date=  $row['post_date'];
-                    $post_image=  $row['post_image'];
-                    $post_content=  $row['post_content'];
+                    $movie_name=  $row['movie_name'];
+                    $movie_actor=  $row['movie_actor'];
+                    $date_of_release=  $row['date_of_release'];
+                    $image=  $row['image'];
+                    $content=  $row['content'];
                    ?>
                    
+<!--
                     <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
 
+                   
+-->
                 <!-- First Blog Post -->
+                <div class="blog-post-1">
                 <h2>
-                    <a href="#"><?php echo $post_title?></a>
+                    <a href="posts.php?p_id=<?php echo $movie_id; ?>"><?php echo $movie_name?></a>
                 </h2>
+                
                 <p class="lead">
-                    by <a href="index.php"><?php echo $post_author ?></a>
+                    by <a href="index.php"><?php echo $movie_actor ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image;?>" alt="">
-                <hr>
-                <p><?php echo $post_content?></p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $date_of_release?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?php echo $image;?>" alt="">
+                    
+                    <hr>
+                <p><?php echo $content?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
+                
+                    
+                    
+              </div>
+                
+                
 
                 <?php }
 
@@ -88,36 +98,44 @@ include "include/navigation.php"
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
+<!--                        <img class="media-object" src="http://placehold.it/64x64" alt="">-->
                     </a>
                     <div class="media-body">
+<!--
                         <h4 class="media-heading">Start Bootstrap
                             <small>August 25, 2014 at 9:30 PM</small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+-->
+<!--                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.-->
                     </div>
                 </div>
 
                 <!-- Comment -->
                 <div class="media">
-                    <a class="pull-left" href="#">
+                    <a class="pull-left" href="#"></a>
+<!--
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
+-->
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
+<!--
+                        <h4 class="media-heading">Start Watching
                             <small>August 25, 2014 at 9:30 PM</small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+-->
+<!--                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.-->
                         <!-- Nested Comment -->
                         <div class="media">
                             <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
+<!--                                <img class="media-object" src="http://placehold.it/64x64" alt="">-->
                             </a>
                             <div class="media-body">
+<!--
                                 <h4 class="media-heading">Nested Start Bootstrap
                                     <small>August 25, 2014 at 9:30 PM</small>
                                 </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+-->
+<!--                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.-->
                             </div>
                         </div>
                         <!-- End Nested Comment -->
