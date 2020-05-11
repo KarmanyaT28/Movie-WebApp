41267137e4f618feea861baa2ab19111ec9044e0
karmanya-crypto <61420434+karmanya-crypto@users.noreply.github.com>
2020-05-11 15:40:09 +0530

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -24,22 +24,194 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            WELCOME TO ADMIN
+                          <b style="color:dark-green;font-family:courier;text-align:center;font-size:200%;">WELCOME TO ADMIN </b> 
                             
-                            <?php echo $_SESSION['username']?>
-                            <small>Subheading</small>
+                            
+                            <small><?php $_SESSION['username'];?></small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Blank Page
-                            </li>
-                        </ol>
+                        
                     </div>
                 </div>
                 <!-- /.row -->
+                
+                <div class="row">
+    <div class="col-lg-3 col-md-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-file-text fa-5x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                      
+                      <?php 
+
+                        $query = "SELECT * FROM posts";
+                        $select_all_post = mysqli_query($connection,$query);
+                        $post_count = mysqli_num_rows($select_all_post);
+
+                      echo  "<div class='huge'>{$post_count}</div>"
+
+                        ?>
+
+
+                        <div>Posts</div>
+                    </div>
+                </div>
+            </div>
+            <a href="posts.php">
+                <div class="panel-footer">
+                    <span class="pull-left">View Details</span>
+                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="clearfix"></div>
+                </div>
+            </a>
+        </div>
+    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+           
+                                      <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                       <?php 
+
+                                        $query = "SELECT * FROM users";
+                                        $select_all_users = mysqli_query($connection,$query);
+                                        $user_count = mysqli_num_rows($select_all_users);
+
+                                      echo  "<div class='huge'>{$user_count}</div>"
+
+                                        ?>
+
+                                       
+                                        <div> Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="users.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-list fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+
+                                     <?php 
+
+                                    $query = "SELECT * FROM categories";
+                                    $select_all_categories = mysqli_query($connection,$query);
+                                    $category_count = mysqli_num_rows($select_all_categories);
+
+                                  echo  "<div class='huge'>{$category_count}</div>"
+
+                                    ?>
+
+                                   <div>Categories</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="category.php">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+                
+                
+                
+<div class="row">
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+      google.charts.load('current', {'packages':['gauge']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Label', 'Value'],
+          ['Activeusers India', 80],
+          ['Activeusers USA', 55],
+          ['Activeusers worldwide', 68]
+        ]);
+
+        var options = {
+          width: 1500, height: 320,
+          redFrom: 90, redTo: 100,
+          yellowFrom:75, yellowTo: 90,
+          minorTicks: 5
+        };
+
+        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+
+        setInterval(function() {
+          data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 13000);
+        setInterval(function() {
+          data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+          chart.draw(data, options);
+        }, 5000);
+        setInterval(function() {
+          data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+          chart.draw(data, options);
+        }, 26000);
+      }
+    </script>
+                   
+                    
+    <div id="chart_div" style="width: 800px; height: 320px;"></div> 
+                    
+                    
+                    
+                    
+                    
+                </div>
+                
+                
 
             </div>
             <!-- /.container-fluid -->
@@ -54,3 +226,4 @@
               <!-- /#page-wrapper -->
 
    <?php include "include/footer.php" ?>
+index.php
